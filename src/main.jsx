ae7a54aee313fe4ef3a5b7dade47c9bafe1be762
Pawlink26 <logannicolebiz@gmail.com>
2026-05-19 14:42:10 -0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RescuPawLink from './RescuPawLink.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RescuPawLink />
+  </React.StrictMode>,
+)
